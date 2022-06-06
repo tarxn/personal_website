3db3a9fa9bf315996aba77e95435267ea29fafd1
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-06-06 18:39:11 +0530

    final home page

---
+++ footer.php
@@ -0,0 +1,37 @@
+<div class="container-fluid" style="background-color: maroon; height: 165px;">
+        <div class="container text-white-muted">
+        <footer >
+            <div class="left-col container " >
+                <img src="https://scai.iitd.ac.in/iitd_logo.png" alt="logo" width="auto" height="100"
+                        class="d-inline-block align-text-center">
+            </div>
+            <div class="center-col container"> 
+                <h4>IIT Delhi</h4>
+                <h7>Hauz Khas, New Delhi 110016</h7>
+                <p style="font-size: 14px;">abc@xyz.com
+                    sdkvfsfd<br>
+                    sdfljsd<br>
+                </p>
+            </div> 
+       
+    
+                
+            </footer> 
+        </div>
+
+    </div>
+<script src="js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript">
+        var nav = document.querySelector('nav');
+
+        window.addEventListener('scroll', function () {
+            if (window.pageYOffset > 100) {
+                nav.classList.add('bg-maroon', 'shadow');
+            } else {
+                nav.classList.remove('bg-maroon', 'shadow');
+            }
+        });
+    </script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Personal website</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <style>
+        .banner-image {
+            background-size: cover;
+            max-height: 300px;
+        }
+
+        .bg-maroon {
+            background-color: #8c1515 !important
+        }
+
+        #active_page {
+            border-bottom: #ffee06 solid 2px;
+        }
+
+        .vl {
+            border-left: #ffffff solid 1px;
+            height: 20px;
+        }
+
+        .inline {
+            display: inline-block;
+        }
+
+        .border_card {
+            border-radius: 1%;
+            width: 99.999%;
+            height: auto;
+            background-color: #edebeb;
+        }
+        .left-col, .right-col{
+            width:12.5%;
+            float: left;
+        }
+        .center-col{
+            width:50%;
+            float:left;
+        }
+
+        footer {
+            text-align: left;
+            margin: auto;
+            color: rgb(229, 229, 229);
+            position: relative;
+            padding-top: 30px;
+            padding-bottom: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Navbar  -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-2">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="https://scai.iitd.ac.in/iitd_logo.png" alt="logo" width="auto" height="45"
+                    class="d-inline-block align-text-center"></a>
+            <a class="navbar-brand align-text-center" href="#" style="color: white; font-size:25px;"> IIT Delhi <div
+                    class="vl inline"></div> Researcher </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="mx-auto"></div>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" id="active_page"   href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Publication</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle active " href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
