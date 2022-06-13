82871eeaa2fe18eced9015673ce3d98c2833efcc
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-06-14 00:30:25 +0530

    cum it

---
+++ gallery.php
@@ -0,0 +1,38 @@
+<?php
+include("header.php");
+?>
+
+<div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center" style="background-image: url('images/1836325.jpg');">
+    <div class="content text-center">
+        <h1 class="text-white">Gallery</h1>
+    </div>
+</div>
+
+<!-- Main Content Area -->
+
+<div class="container my-5 d-grid gap-5">
+    <div>
+        <div class="container-fluid">
+            <h3 style="color:blue">
+
+                Sponsored research projects as Principal Investigator
+
+            </h3>
+        </div>
+        <div class="p-5 border_card">
+            <img src="images/88408.jpg" height="auto" width="1200">
+            <img class="my-5" src="images/674334.jpg" height="auto" width="1200">
+        </div>
+    </div>
+
+
+
+    
+
+</div>
+
+
+
+<?php
+include("footer.php");
+?>
