b389255deb082ee1f0f3fdce74e7e65fb5f18100
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-06-13 23:02:35 +0530

    pub.

--- publication.php
+++ publication.php
@@ -10,39 +10,82 @@
 </div>
 
 <!-- Main Content Area -->
-<div class="container my-5 d-grid gap-5">
-    <div class="p-5 border_card">
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
-            natus iusto fugit id saepe neque rerum magni laudantium accusantium
-            dolorem numquam quasi.
-        </p>
-    </div>
-    <div class="p-5 border_card">
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
-            natus iusto fugit id saepe neque rerum magni laudantium accusantium
-            dolorem numquam quasi.
-        </p>
-    </div>
-    <div class="p-5 border_card">
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
-            natus iusto fugit id saepe neque rerum magni laudantium accusantium
-            dolorem numquam quasi.
-        </p>
-    </div>
-    <div class="p-5 border_card">
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
-            natus iusto fugit id saepe neque rerum magni laudantium accusantium
-            dolorem numquam quasi. df
-        </p>
-    </div>
+<div class="container-fluid my-5 gap-5 border_card">
+<h2 class="p-3" style="color:blue;">Section 1</h2>
+    <div class="container ">
+        <div class="p-2">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-2 ">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-2">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-2">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi. df
+            </p>
+        </div>
+    </div>   
+</div>
+
+
+<div class="container-fluid my-5 gap-5 border_card">
+<h2 class="p-3" style="color:blue;">Section 2</h2>
+    <div class="container ">
+    
+        <div class="p-2">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-2 ">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-2">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-2">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi. df
+            </p>
+        </div>
+    </div> 
 </div>
 
 
