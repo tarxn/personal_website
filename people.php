6ab5569c1959527ca40d5ecff0c26a4b27afdee1
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-06-16 01:54:35 +0530

    ppl commit

--- people.php
+++ people.php
@@ -10,21 +10,37 @@ include("header.php");
 
 <!-- Main Content Area -->
 <div class="container my-5 d-grid gap-5">
+        <?php
+
+            $connection=mysqli_connect("localhost","root","");
+            $db=mysqli_select_db($connection,'personal_website');
+
+            $query="SELECT * FROM `people`";
+            $query_run=mysqli_query($connection,$query);
+
+            while($row=mysqli_fetch_array($query_run))
+            {
+        ?>
+
     <div class="p-5 border_card">
         <div class="left-col ppl_img">
-            <img class="ppl_img" src="images/stock_woman.jpg" class="d-inline-block align-text-center">
+        <?php echo '<img class="ppl_img d-inline-block align-text-center" src="data:image;base64,' . base64_encode($row['image']) . '"  alt="Image" >'; ?>
+            <!-- <img class="ppl_img" src="images/stock_woman.jpg" class="d-inline-block align-text-center"> -->
         </div>
         <div class="center-col">
             <p>
-            <h4>Student name</h4>
-            B.Sc. (Zoology), 2016, Fakir Mohan University, Odisha<br>
-            M.Sc. (Zoology), 2018, North Orissa University, Odisha<br>
-            Project: Role of endothelial plasticity in the tumour microenvironment (UGC-JRF)<br>
-            Email: bt19d025@smail.iitm.ac.in; LinkedIn<br>
-            Hobbies: Gardening, Reading novels, Singing and Dancing<br>
+            <h4><?php echo $row['name']; ?></h4>
+            <?php echo $row['edu1']; ?><br>
+            <?php echo $row['edu2']; ?><br>
+            <?php echo $row['desc1']; ?><br>
+            <?php echo $row['contact']; ?><br>
+            <?php echo $row['desc2']; ?><br>
             </p>
         </div>
     </div>
+    <?php
+        }
+        ?>
     <div class="p-5 border_card">
         <div class="left-col ppl_img">
             <img class="ppl_img" src="images/stock_woman.jpg" class="d-inline-block align-text-center">
