efbed7f197ae2902d7267a1314bb7b5c57137281
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-06-16 00:09:48 +0530

    commit

--- contact.php
+++ contact.php
@@ -13,56 +13,59 @@ include("header.php");
 <div class="container my-5 gap-5">
 
     <div class="form_card box_shadow half_col2 p-5">
-    <div class="pybottom">
-        <h5 style="color:#002a73;">If you are interested to work with us, please share your resume here. We will get back to you.</h5>
-    </div>
-        <form>
+        <div class="pybottom"> 
+            <h5 style="color:#002a73;">If you are interested to work with us, please share your resume here. We will get back to you.</h5>
+        </div>
+        <form action="https://docs.google.com/forms/d/e/1FAIpQLSdHVIJOWUQdD4jLHJ4GI0daPZtWaxQVH-0UZ8HJviDGxgHk_Q/formResponse">
         <div class="form-group p-1 px-3">
                 <label for="exampleFormControlInput1">Your Name</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" >
+                <input type="text" name="entry.704798293" required class="form-control" id="exampleFormControlInput1" >
             </div>
             <div class="form-group p-1 px-3">
                 <label for="exampleFormControlInput1">Your Email</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" >
+                <input type="email" name="entry.510677314" required class="form-control" id="exampleFormControlInput1" >
             </div>
             <div class="form-group p-1 px-3">
                 <label for="exampleFormControlSelect1">Subject</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" >
+                <input type="text" name="entry.1398535834" required class="form-control" id="exampleFormControlInput1" >
             </div>
             <div class="form-group p-1 px-3">
                 <label for="exampleFormControlTextarea1">Message</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <textarea class="form-control" name="entry.519175831" required id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
             <div class="form-group p-1 px-3">
                 <label for="exampleFormControlSelect1">Resume link</label>
-                <input type="link" class="form-control" id="exampleFormControlInput1" >
+                <input type="link" name="entry.509376194" required class="form-control" id="exampleFormControlInput1" >
             </div >
             <div  class="p-4 d-flex justify-content-center">
-            <button type="button" class="btn bg-maroon px-5" style="color:white;">Submit</button></div>
+            <button type="submit" class="btn bg-maroon px-5" style="color:white;">Submit</button></div>
         </form>
+        <!-- <iframe style="width:auto;" class="align-center" src="https://docs.google.com/forms/d/e/1FAIpQLSdHVIJOWUQdD4jLHJ4GI0daPZtWaxQVH-0UZ8HJviDGxgHk_Q/viewform?embedded=true" height="580" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe> -->
 
 
 
     </div>
 
     <div class="half_col1 p-3 form_card_r">
-        
+
         <div>
-            <img src="images/building.png" height="70px" width="auto"> <h2>Location:</h2>
+            <img src="images/building.png" height="70px" width="auto">
+            <h2>Location:</h2>
             <h5>
-            Agrinanobiotechnology Lab, Room number 280, Block 3, <br>IIT Delhi -110016
+                Agrinanobiotechnology Lab, Room number 280, Block 3, <br>IIT Delhi -110016
 
             </h5>
         </div>
-        
-    <br> <br>
+
+        <br> <br>
 
         <div>
-            <img src="images/email.png" height="60px" width="auto"><h2>Email:</h2>
+            <img src="images/email.png" height="60px" width="auto">
+            <h2>Email:</h2>
             <h5>kdashora@rdat.iitd.ac.in
-</h5>
+            </h5>
         </div>
-       
+
     </div>
     <div>
         <h5 style="color:white; padding:1rem;"> Enter Your details to contact us.</h5>
