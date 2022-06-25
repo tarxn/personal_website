e760d12e1d2f6ba192ba9015f7e76b33ec177712
YashJayswal24 <84831969+YashJayswal24@users.noreply.github.com>
2022-06-26 02:16:18 +0530

    commit

--- Research.php
+++ Research.php
@@ -13,57 +13,56 @@ include("header.php");
     <section class="u-clearfix  u-section-2" id="sec-17ca">
         <div class="u-clearfix u-sheet u-sheet-1">
             <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
-                
-                <div class="u-layout" style="">
-                    <div class="u-layout-row" style="">
-                        <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
-                            <div class="u-container-layout u-valign-middle u-container-layout-1">
-                                <h2 class="u-align-center u-text u-text-default u-text-1">Research 1</h2>
-                                <p class="u-align-center u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
-                                <a href="#" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
-                            </div>
-                        </div>
-                        <img class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="images/wall.jpg" data-image-width="1080" data-image-height="1080">
-                        <div class="u-container-layout u-valign-middle u-container-layout-2" src="images/wall.jpg"></div>
-                        </img>
-                    </div>
-                </div>
 
-                <div class="u-layout" style="">
-                    <div class="u-layout-row" style="">
-                        <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
-                            <div class="u-container-layout u-valign-middle u-container-layout-1">
-                                <h2 class="u-align-center u-text u-text-default u-text-1">Research 1</h2>
-                                <p class="u-align-center u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
-                                <a href="#" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
-                            </div>
-                        </div>
-                        <img class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="images/wall.jpg" data-image-width="1080" data-image-height="1080">
-                        <div class="u-container-layout u-valign-middle u-container-layout-2" src="images/wall.jpg"></div>
-                        </img>
-                    </div>
-                </div>
 
-                <div class="u-layout" style="">
-                    <div class="u-layout-row" style="">
-                        <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
-                            <div class="u-container-layout u-valign-middle u-container-layout-1">
-                                <h2 class="u-align-center u-text u-text-default u-text-1">Research 1</h2>
-                                <p class="u-align-center u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
-                                <a href="#" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
+                <?php
+                $connection = mysqli_connect("localhost", "u731109599_kavyadb", "bUH:OXI9GZ]9", "u731109599_kavyadb");
+                $db = mysqli_select_db($connection, '');
+
+                $query = "SELECT * FROM `research`";
+                $query_run = mysqli_query($connection, $query);
+
+
+                while ($row = mysqli_fetch_array($query_run)) {
+                ?>
+
+                    <div class="u-layout" style="">
+                        <div class="u-layout-row" style="">
+                            <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
+                                <div class="u-container-layout u-valign-middle u-container-layout-1">
+                                    <h2 class="u-align-center u-text u-text-default u-text-1">
+                                        <?php
+                                        echo $row['title'];
+                                        ?>
+                                    </h2>
+                                    <p class="u-align-center u-text u-text-2"><?php
+                                        echo $row['description'];
+                                        ?></p>
+                                    <a href="<?php echo $row['link']; ?>" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
+                                </div>
                             </div>
+                            <?php 
+                            echo '<img src="data:image;base64,' . base64_encode($row['images']) . '"  alt="Image" data-image-width="1080" data-image-height="1080">';
+                            ?>
+                            <div class="u-container-layout u-valign-middle u-container-layout-2" src="images/wall.jpg"></div>
+                            </img>
                         </div>
-                        <img class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="images/wall.jpg" data-image-width="1080" data-image-height="1080">
-                        <div class="u-container-layout u-valign-middle u-container-layout-2" src="images/wall.jpg"></div>
-                        </img>
                     </div>
-                </div>
+                <?php
+                }
+                mysqli_close($connection);
+                ?>
+
+
+
+
+
             </div>
         </div>
 
-        
+
     </section>
- 
+
 </div>
 
 
