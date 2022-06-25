c0c7408e819865a1c71cc57c59eddbcadb9f54f9
YashJayswal24 <84831969+YashJayswal24@users.noreply.github.com>
2022-06-26 02:03:55 +0530

    commit

--- Research.php
+++ Research.php
@@ -13,6 +13,37 @@ include("header.php");
     <section class="u-clearfix  u-section-2" id="sec-17ca">
         <div class="u-clearfix u-sheet u-sheet-1">
             <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
+                
+                <div class="u-layout" style="">
+                    <div class="u-layout-row" style="">
+                        <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
+                            <div class="u-container-layout u-valign-middle u-container-layout-1">
+                                <h2 class="u-align-center u-text u-text-default u-text-1">Research 1</h2>
+                                <p class="u-align-center u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+                                <a href="#" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
+                            </div>
+                        </div>
+                        <img class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="images/wall.jpg" data-image-width="1080" data-image-height="1080">
+                        <div class="u-container-layout u-valign-middle u-container-layout-2" src="images/wall.jpg"></div>
+                        </img>
+                    </div>
+                </div>
+
+                <div class="u-layout" style="">
+                    <div class="u-layout-row" style="">
+                        <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
+                            <div class="u-container-layout u-valign-middle u-container-layout-1">
+                                <h2 class="u-align-center u-text u-text-default u-text-1">Research 1</h2>
+                                <p class="u-align-center u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+                                <a href="#" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
+                            </div>
+                        </div>
+                        <img class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="images/wall.jpg" data-image-width="1080" data-image-height="1080">
+                        <div class="u-container-layout u-valign-middle u-container-layout-2" src="images/wall.jpg"></div>
+                        </img>
+                    </div>
+                </div>
+
                 <div class="u-layout" style="">
                     <div class="u-layout-row" style="">
                         <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
@@ -29,70 +60,10 @@ include("header.php");
                 </div>
             </div>
         </div>
+
+        
     </section>
- <hr>
-    <section class="u-clearfix  u-section-2" id="sec-17ca">
-        <div class="u-clearfix u-sheet u-sheet-1">
-            <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
-                <div class="u-layout" style="">
-                    <div class="u-layout-row" style="">
-                        <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
-                            <div class="u-container-layout u-valign-middle u-container-layout-1">
-                                <h2 class="u-align-center u-text u-text-default u-text-1">Research 1</h2>
-                                <p class="u-align-center u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
-                                <a href="#" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
-                            </div>
-                        </div>
-                        <img class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="images/wall.jpg" data-image-width="1080" data-image-height="1080">
-                        <div class="u-container-layout u-valign-middle u-container-layout-2" src="images/wall.jpg"></div>
-                        </img>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <hr>
-    <section class="u-clearfix u-section-2" id="sec-17ca">
-        <div class="u-clearfix u-sheet u-sheet-1">
-            <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
-                <div class="u-layout" style="">
-                    <div class="u-layout-row" style="">
-                        <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
-                            <div class="u-container-layout u-valign-middle u-container-layout-1">
-                                <h2 class="u-align-center u-text u-text-default u-text-1">Research 1</h2>
-                                <p class="u-align-center u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
-                                <a href="#" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
-                            </div>
-                        </div>
-                        <img class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="images/wall.jpg" data-image-width="1080" data-image-height="1080">
-                        <div class="u-container-layout u-valign-middle u-container-layout-2" src="images/wall.jpg"></div>
-                        </img>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <hr>
-    <section class="u-clearfix  u-section-2" id="sec-17ca">
-        <div class="u-clearfix u-sheet u-sheet-1">
-            <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
-                <div class="u-layout" style="">
-                    <div class="u-layout-row" style="">
-                        <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
-                            <div class="u-container-layout u-valign-middle u-container-layout-1">
-                                <h2 class="u-align-center u-text u-text-default u-text-1">Research 1</h2>
-                                <p class="u-align-center u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
-                                <a href="#" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
-                            </div>
-                        </div>
-                        <img class="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="images/wall.jpg" data-image-width="1080" data-image-height="1080">
-                        <div class="u-container-layout u-valign-middle u-container-layout-2" src="images/wall.jpg"></div>
-                        </img>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+ 
 </div>
 
 
