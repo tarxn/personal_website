507dc01a1687d092b6af95ae7d1576962bc39562
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-06-26 02:43:59 +0530

    c

--- Research.php
+++ Research.php
@@ -10,22 +10,20 @@ include("header.php");
 
 <!-- Main Content Area -->
 <div class="u-body u-xl-mode u-grey-5 my-5 gap-5">
-    <section class="u-clearfix  u-section-2" id="sec-17ca">
-        <div class="u-clearfix u-sheet u-sheet-1">
-            <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
+
+    <?php
+    $connection = mysqli_connect("localhost", "u731109599_kavyadb", "bUH:OXI9GZ]9", "u731109599_kavyadb");
+    $db = mysqli_select_db($connection, '');
+
+    $query = "SELECT * FROM `research`";
+    $query_run = mysqli_query($connection, $query);
 
 
-                <?php
-                $connection = mysqli_connect("localhost", "u731109599_kavyadb", "bUH:OXI9GZ]9", "u731109599_kavyadb");
-                $db = mysqli_select_db($connection, '');
-
-                $query = "SELECT * FROM `research`";
-                $query_run = mysqli_query($connection, $query);
-
-
-                while ($row = mysqli_fetch_array($query_run)) {
-                ?>
-
+    while ($row = mysqli_fetch_array($query_run)) {
+    ?>
+        <section class="u-clearfix  u-section-2" id="sec-17ca">
+            <div class="u-clearfix u-sheet u-sheet-1">
+                <div class="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
                     <div class="u-layout" style="">
                         <div class="u-layout-row" style="">
                             <div class="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
@@ -41,25 +39,26 @@ include("header.php");
                                     <a href="<?php echo $row['link']; ?>" class="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">Read more</a>
                                 </div>
                             </div>
-                            <div class="container u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src=""  style="max-width: 200px;">
-                            <?php echo '<img class="d-inline-block" src="data:image;base64,' . base64_encode($row['image']) . '"  alt="Image"  height="auto" >'; ?></div>
-                            </div>
+                            <div class="container u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="" style="max-width: 200px;">
+                                <?php echo '<img class="d-inline-block" src="data:image;base64,' . base64_encode($row['image']) . '"  alt="Image"  height="auto" >'; ?></div>
                         </div>
                     </div>
-                <?php
-                }
-                mysqli_close($connection);
-                ?>
+                </div>
+
 
 
 
 
 
             </div>
-        </div>
+</div>
 
 
-    </section>
+</section>
+<?php
+    }
+    mysqli_close($connection);
+?>
 
 </div>
 
