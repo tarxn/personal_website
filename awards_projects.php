155da43b84e399279404ca12d56bd3e939e5b21b
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-06-27 08:47:35 +0530

    df

--- awards_projects.php
+++ awards_projects.php
@@ -18,23 +18,23 @@ include("header.php");
     </h3>
 </div>
 
-    <div class="p-5 border_card ">
+    <div class="p-3 border_card ">
     <div class="container">
         <ul>
             <li>
-                <h5>
+                <p>
                     Best innovation award- UNDP-2021.
-                </h5>
+                </p>
             </li>
             <li>
-                <h5>
+                <p>
                     Innovation in Food award- PETA-2020.
-                </h5>
+                </p>
             </li>
             <li>
-                <h5>
+                <p>
                     Best Women Scientist Award-SRBS-2020.
-                </h5>
+                </p>
             </li>
 
 
@@ -54,7 +54,7 @@ include("header.php");
 
     </h3>
 </div>
-<div class="p-5 my-5 border_card">
+<div class="p-3 my-3 border_card">
     <?php
 
     $connection = mysqli_connect("localhost", "u731109599_kavyadb", "bUH:OXI9GZ]9", "u731109599_kavyadb");
