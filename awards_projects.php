8b50b9cbbc8e7bf03c61eb9ce545f5152ff46b48
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-06-27 08:45:19 +0530

    c

--- awards_projects.php
+++ awards_projects.php
@@ -16,26 +16,32 @@ include("header.php");
         Awards
 
     </h3>
-    </div>
-    <div class="container-fluid">
-        <div class="p-5 border_card ">
-            <ul>
-               <li> <h4>
+</div>
+<div class="container">
+    <div class="p-5 border_card ">
+        <ul>
+            <li>
+                <h5>
                     Best innovation award- UNDP-2021.
-                </h4></li>
-                <li><h4>
+                </h5>
+            </li>
+            <li>
+                <h5>
                     Innovation in Food award- PETA-2020.
-                </h4></li>
-                <li><h4>
+                </h5>
+            </li>
+            <li>
+                <h5>
                     Best Women Scientist Award-SRBS-2020.
-                </h4></li>
+                </h5>
+            </li>
 
 
-            </ul>
+        </ul>
 
 
 
-        
+
     </div>
 </div>
 
@@ -46,7 +52,8 @@ include("header.php");
         Projects
 
     </h3>
-
+</div>
+<div class="p-5 my-5 border_card">
     <?php
 
     $connection = mysqli_connect("localhost", "u731109599_kavyadb", "bUH:OXI9GZ]9", "u731109599_kavyadb");
@@ -59,13 +66,14 @@ include("header.php");
     while ($row = mysqli_fetch_array($query_run)) {
     ?>
 
-        <div class="p-5 my-5 border_card">
-            <h3><?php echo $row['description']; ?></h3>
-        </div>
+        <ul>
+           <li> <p><?php echo $row['description']; ?></p></li>
 
+        </ul>
     <?php
     }
     ?>
+
 </div>
 
 
