ddc57a21e00b05e139ed34539c0e0d4389962ca9
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-07-06 18:28:19 +0530

    d

--- people.php
+++ people.php
@@ -24,22 +24,6 @@ include("header.php");
 
     while ($row = mysqli_fetch_array($query_run)) {
     ?>
-<div class="card mb-3" style="max-width: 100%;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
 
         <div class="p-5 border_card row d-flex">
             <div class="col-4 p-0 order-1" style="z-index:1;">
