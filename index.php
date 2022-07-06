d88a0e45ab7f418bb092d50d51593367edc1de02
YashJayswal24 <84831969+YashJayswal24@users.noreply.github.com>
2022-07-06 19:47:13 +0530

    commit

--- index.php
+++ index.php
@@ -41,12 +41,31 @@ include("header.php");
     <h3>Scrolling News and Events</h3>
     <div class="p-5 my-5 border_card" style="max-height:380px ;">
         <marquee behavior="scroll" direction="up" scrollamount="3" style="max-height:320px ;">
-            <h5>IITD AA President Dr. Kalpen Shukla facilitated Prof. Vijay on eve on AGM and Award event on 30.4.2022 </h5><br>
-            <h5>Prof. Vijay has been appointed as Member of Science and Engineering Research Board (SERB), DST, India, 16th March 2022 </h5><br>
-            <h5>Prof. Vijay has been awarded India Green Energy Awards, 2020 by Indian Federation of Green Energy (IFGE) </h5>
-            <br><a href="pics/vijay.pdf" target="_blank">
-                <h5>Prof. Vijay has received Pt Deen Dayal Upadhyaya Recognition for Re-Engineering INDIA 2020 </h5>
-            </a><br>
+
+
+
+            <?php
+
+            $connection = mysqli_connect("localhost", "u731109599_kavyadb", "bUH:OXI9GZ]9", "u731109599_kavyadb");
+            $db = mysqli_select_db($connection, '');
+            $query = "SELECT * FROM `news`";
+            $query_run = mysqli_query($connection, $query);
+
+
+            while ($row = mysqli_fetch_array($query_run)) {
+            ?>
+
+
+
+
+                <h5><?php echo $row['news']; ?> </h5><br>
+
+            <?php
+            }
+            ?>
+
+
+
             <br>
             <br>
         </marquee>
