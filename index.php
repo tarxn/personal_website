e36d91982ac652fe6540258b4ef8e801ced3d11c
YashJayswal24 <84831969+YashJayswal24@users.noreply.github.com>
2022-07-06 19:30:06 +0530

    commit

--- index.php
+++ index.php
@@ -39,14 +39,15 @@ include("header.php");
 
 <div class="container">
     <h3>Scrolling News and Events</h3>
-    <div class="p-5 my-5 border_card" style="max-height:60px ;">
-        <marquee behavior="scroll" direction="up" scrollamount="3" style="max-height:60px ;">
+    <div class="p-5 my-5 border_card" style="max-height:200px ;">
+        <marquee behavior="scroll" direction="up" scrollamount="3" style="max-height:170px ;">
             <h5>IITD AA President Dr. Kalpen Shukla facilitated Prof. Vijay on eve on AGM and Award event on 30.4.2022 </h5><br>
             <h5>Prof. Vijay has been appointed as Member of Science and Engineering Research Board (SERB), DST, India, 16th March 2022 </h5><br>
             <h5>Prof. Vijay has been awarded India Green Energy Awards, 2020 by Indian Federation of Green Energy (IFGE) </h5>
             <br><a href="pics/vijay.pdf" target="_blank">
                 <h5>Prof. Vijay has received Pt Deen Dayal Upadhyaya Recognition for Re-Engineering INDIA 2020 </h5>
             </a><br>
+            <br>
             <br>
         </marquee>
 
