0840449ab3b79730c5e4580882fad67f313e582d
Tarun Bijanapally <ee3200588@iitd.ac.in>
2022-07-06 19:05:43 +0530

    kjgv

--- people.php
+++ people.php
@@ -10,10 +10,10 @@ include("header.php");
 
 <!-- Main Content Area -->
 <div class="container my-3 d-grid gap-3">
-    <h3 style="color:blue">
+    <h4 >
 
         People
-    </h3>
+    </h4>
     <?php
     $connection = mysqli_connect("localhost", "u731109599_kavyadb", "bUH:OXI9GZ]9", "u731109599_kavyadb");
     $db = mysqli_select_db($connection, '');
@@ -73,18 +73,17 @@ include("header.php");
     while ($row = mysqli_fetch_array($query_run)) {
     ?>
 
-        <div class="p-5 border_card row d-flex">
-            <div class="col-4 p-0 order-1" style="z-index:1;">
-                <div>
-                    <?php echo '<img class="d-inline-block" src="data:image;base64,' . base64_encode($row['Image']) . '"  alt="Image" width="220px" height="auto" >'; ?>
-
+        <div class="p-4 card mb-3" style="max-width: 100%;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <?php echo '<img class="d-inline-block" src="data:image;base64,' . base64_encode($row['image']) . '"  alt="Image" width="220px" height="auto" >'; ?>
                 </div>
-            </div>
-            <div class="col-8 py-3 order-2" style="z-index:1; position:relative; ">
-
-                <h4><?php echo $row['Name']; ?></h4>
-
-
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h4 class="card-title"><?php echo $row['name']; ?></h4>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     <?php
@@ -110,14 +109,13 @@ include("header.php");
     while ($row = mysqli_fetch_array($query_run)) {
     ?>
 
-        <div class="p-5 border_card row d-flex">
-            <div class="col-4 p-0 order-1" style="z-index:1;">
-                <div>
-                    <?php echo '<img class="d-inline-block" src="data:image;base64,' . base64_encode($row['image']) . '"  alt="Image" width="220px" height="auto" >'; ?>
-
+<div class="p-4 card mb-3" style="max-width: 100%;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <?php echo '<img class="d-inline-block" src="data:image;base64,' . base64_encode($row['image']) . '"  alt="Image" width="220px" height="auto" >'; ?>
                 </div>
-            </div>
-            <div class="col-8 py-3 order-2" style="z-index:1; position:relative; ">
+                <div class="col-md-8">
+                    <div class="card-body">
 
                 <h4><?php echo $row['name']; ?></h4>
                 <p>
@@ -129,8 +127,11 @@ include("header.php");
                     <a href="<?php echo $row['researchgate']; ?>" class="link-primary"> Research Gate </a><br>
                 </p>
 
+                </div>
+                </div>
             </div>
         </div>
+
     <?php
     }
     ?>
