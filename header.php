df5d42d6165f3145f7e4474e2ab00803a31fb077
Bijanapally <ee3200588@iitd.ac.in>
2023-03-01 22:46:08 +0530

    mes

--- header.php
+++ header.php
@@ -171,7 +171,7 @@
                     </li>
 
 
-                    <!-- <li class="nav-item dropdown">
+                    <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown
@@ -182,7 +182,7 @@
                         </ul>
                     </li> -->
 
-                    <li class="nav-item">
+                    <!-- <li class="nav-item">
                         <a class="nav-link" href="contact.php">Contact us</a>
                     </li>
                 </ul>
