55709c44910351dbe25e2ea7ae08ca6355941cb7
Bijanapally <ee3200588@iitd.ac.in>
2023-03-01 23:07:33 +0530

    mes

--- header.php
+++ header.php
@@ -166,58 +166,60 @@
 <body class="offcanvas-active">
     <!-- Navbar  -->
 
-    <nav class="fixed-top topnav navbar-expand-lg navbar scrolling-off p-md-2">
-        <div class="container dmmy ">
-            <h1 class="navbar-brand align-text-center text-white" href="#" style=" font-size: 30px; font-family: 'Lobster Two', cursive;">Prof. Kavya Dashora </h1>
-            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button> -->
-            <div class="container-fluid" id="myLinks" style="background-color: #008080; height: auto; color:#fff;">
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
+    <nav class="fixed-top navbar-expand-lg navbar scrolling-off p-md-2">
+        <div class="topnav">
+            <div class="container dmmy ">
+                <h1 class="navbar-brand align-text-center text-white" href="#" style=" font-size: 30px; font-family: 'Lobster Two', cursive;">Prof. Kavya Dashora </h1>
+                <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button> -->
+                <div class="container-fluid" id="myLinks" style="background-color: #008080; height: auto; color:#fff;">
+                    <a href="#news">News</a>
+                    <a href="#contact">Contact</a>
+                    <a href="#about">About</a>
+                </div>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <!-- <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="mx-auto"></div>
+                    <ul class="navbar-nav" style="color: blue;">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="people.php">People</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="publication.php">Publication</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Research.php">Research</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="awards_projects.php">Awards & Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="gallery.php">Gallery</a>
+                        </li>
+
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                            </ul>
+                        </li> -->
+
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="contact.php">Contact us</a>
+                        </li>
+                    </ul>
+                </div> -->
             </div>
-            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-                <i class="fa fa-bars"></i>
-            </a>
-            <!-- <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="mx-auto"></div>
-                <ul class="navbar-nav" style="color: blue;">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="people.php">People</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="publication.php">Publication</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="Research.php">Research</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="awards_projects.php">Awards & Projects</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="gallery.php">Gallery</a>
-                    </li>
-
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                        </ul>
-                    </li> -->
-
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact us</a>
-                    </li>
-                </ul>
-            </div> -->
         </div>
     </nav>
